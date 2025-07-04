22480ef33ec9161a9b6f1f93942ede12d72ca38b
이호연 <ihoyeon@ihoyeon-ui-MacBookAir.local>
2025-07-04 15:17:11 +0900

    초기세팅

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div className="text-3xl font-bold underline">
+      내가 다 해줘따 화이팅 유빈민규~
+    </div>
+  );
+};
+
+export default App;
